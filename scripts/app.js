6f83ce3001a8a45e9192cf6ae53f7ec2735a9aba
Danny JeanLouis <dlouis3386@gmail.com>
2022-01-04 22:05:53 -0500

    add responsiveness to navigation

--- scripts/app.js
+++ scripts/app.js
@@ -4,18 +4,19 @@ const menuCloseBtn = document.querySelector(".navbar__menu-btn--close");
 const navbarMenu = document.querySelector(".navbar__navigation");
 const overlay = document.querySelector(".overlay");
 
-const toggleOverlay = () => overlay.classList.toggle("overlay--active");
+const toggleMenuWithOverlay = () => {
+  overlay.classList.toggle("overlay--active");
+  navbarMenu.classList.toggle("navbar__navigation--active");
+};
 
-menuOpenBtn.addEventListener("click", (evt) => {
-  toggleOverlay();
+menuOpenBtn.onclick = function (evt) {
+  toggleMenuWithOverlay();
   evt.currentTarget.style.display = "none";
   menuCloseBtn.classList.toggle("navbar__menu-btn--close");
-  navbarMenu.classList.toggle("navbar__navigation--active");
-});
+};
 
-menuCloseBtn.addEventListener("click", (evt) => {
-  toggleOverlay();
+menuCloseBtn.onclick = function (evt) {
+  toggleMenuWithOverlay();
   evt.currentTarget.classList.toggle("navbar__menu-btn--close");
   menuOpenBtn.style.display = "block";
-  navbarMenu.classList.toggle("navbar__navigation--active");
-});
+};
