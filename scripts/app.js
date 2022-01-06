db4535fb55e790adac8b445af3f239ac79a32c8c
Danny JeanLouis <dlouis3386@gmail.com>
2022-01-06 15:58:51 -0500

    add header image responsive js logic

--- scripts/app.js
+++ scripts/app.js
@@ -3,6 +3,11 @@ const menuOpenBtn = document.querySelector(".navbar__menu-btn--open");
 const menuCloseBtn = document.querySelector(".navbar__menu-btn--close");
 const navbarMenu = document.querySelector(".navbar__navigation");
 const overlay = document.querySelector(".overlay");
+const createGraphic = document.querySelector(".header__create-graphic");
+const beautifulStories = document.querySelector(
+  ".header__beautiful-stories-graphic"
+);
+const designedGraphic = document.querySelector(".header__designed-graphic");
 
 const toggleMenuWithOverlay = () => {
   overlay.classList.toggle("overlay--active");
@@ -20,3 +25,48 @@ menuCloseBtn.onclick = function (evt) {
   evt.currentTarget.classList.toggle("navbar__menu-btn--close");
   menuOpenBtn.style.display = "block";
 };
+
+/* Code for responsive header images begin */
+const getClass = (element) => {
+  let classValue = "";
+
+  switch (element) {
+    case createGraphic:
+      classValue = "create-and-share";
+      break;
+    case beautifulStories:
+      classValue = "beautiful-stories";
+      break;
+    case designedGraphic:
+      classValue = "designed-for-everyone";
+      break;
+  }
+
+  return classValue;
+};
+
+const setGraphic = (graphic) => {
+  if (window.innerWidth >= 1440) {
+    graphic.setAttribute(
+      "src",
+      `./assets/home/desktop/${getClass(graphic)}.jpg`
+    );
+  } else if (window.innerWidth >= 768) {
+    graphic.setAttribute(
+      "src",
+      `./assets/home/tablet/${getClass(graphic)}.jpg`
+    );
+  } else {
+    graphic.setAttribute(
+      "src",
+      `./assets/home/mobile/${getClass(graphic)}.jpg`
+    );
+  }
+};
+
+window.addEventListener("resize", (evt) => {
+  setGraphic(createGraphic);
+  setGraphic(beautifulStories);
+  setGraphic(designedGraphic);
+});
+/* Code for responsive header images ends */
